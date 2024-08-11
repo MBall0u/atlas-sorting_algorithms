af08e975767f88bc29ead62adf3fa4aebadb3997
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-11 17:18:20 -0500

    M - 5:13 PM - made file for selection_sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * selection_sort - see below
+ * Description - iterates from the beginning to the end
+ * of the array, finding the lowest value and then moving
+ * it to the sorted portion, then it shortens the searchable
+ * array and repeats till sorted
+ * @array: the given unsorted array
+ * @size: size of the array
+ * Return: Void
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t pos, pos_l, pos_h = size - 1, hold = 0;
+	int temp;
+
+	for (pos_l = 0; pos_l != pos_h; pos_l++)
+	{
+		for (pos = pos_l; pos < size; pos++)
+		{
+			if (array[pos] < array[hold])
+				hold = pos;
+		}
+		temp = array[pos_l];
+		array[pos_l] = array[hold];
+		array[hold] = temp;
+		print_array(array, size);
+	}
+}
